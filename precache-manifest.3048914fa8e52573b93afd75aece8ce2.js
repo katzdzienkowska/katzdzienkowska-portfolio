62114aaa43635c20fd34fb667643d1c354d3ee9a
Kat Zdzienkowska <katzdzienkowska@kats-mbp.home>
2022-07-10 17:55:35 +0100

    Updates

---
+++ precache-manifest.3048914fa8e52573b93afd75aece8ce2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7b9c88c608420bf44fad",
+    "url": "/katzdzienkowska-portfolio/static/css/main.58b63981.chunk.css"
+  },
+  {
+    "revision": "7b9c88c608420bf44fad",
+    "url": "/katzdzienkowska-portfolio/static/js/main.7b9c88c6.chunk.js"
+  },
+  {
+    "revision": "0d630a3e1f21db7817dc",
+    "url": "/katzdzienkowska-portfolio/static/js/1.0d630a3e.chunk.js"
+  },
+  {
+    "revision": "7d89464741f2193926da",
+    "url": "/katzdzienkowska-portfolio/static/js/2.7d894647.chunk.js"
+  },
+  {
+    "revision": "4ab7113cdb22cb50c605",
+    "url": "/katzdzienkowska-portfolio/static/js/runtime~main.4ab7113c.js"
+  },
+  {
+    "revision": "9b6ae08abd659c2bc47d7cec6db3dbf0",
+    "url": "/katzdzienkowska-portfolio/index.html"
+  }
+];
